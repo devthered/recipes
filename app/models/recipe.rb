1dd19a4bbe8a43abe403196fb92d054999c6f6cd
Devin Reed <reed.devin@gmail.com>
2019-06-24 21:33:32 +0100

    Adds basic add functionality for Recipes.
    Fixed up input fields on edit/add form for Recipe model.
    Added input sanitation for ingredient list and instructions.
    Removed log directory from git.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,16 @@ class Recipe
   field :ingredients, type: Array
   field :instructions, type: String
   field :genre, type: String
+
+  def ingredients=(ingredients)
+    if ingredients.respond_to?('split')
+      self[:ingredients] = ingredients.sub("\r", '').split("\n")
+    else
+      self[:ingredients] = ingredients
+    end
+  end
+
+  def instructions=(instructions)
+    self[:instructions] = instructions.sub("\n", '<br><br>')
+  end
 end
