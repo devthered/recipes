e307ae1c2c58f77c95868c3eac12b31641436cec
Devin Reed <reed.devin@gmail.com>
2019-06-26 13:37:03 +0100

    Render recipes using partial. Implemented recipe#show as well as #index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,9 +4,9 @@ class Recipe
 
   field :title, type: String
   field :source, type: String
+  field :genre, type: String
   field :ingredients, type: Array
   field :instructions, type: String
-  field :genre, type: String
 
   def ingredients=(ingredients)
     if ingredients.respond_to?('split')
