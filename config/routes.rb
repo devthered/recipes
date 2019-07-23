25bfa514c0dbc2475673f25660c9c9bb649aff0f
Devin Reed <reed.devin@gmail.com>
2019-07-23 22:42:20 +0100

    Made login page. Handle login errors but not success case yet. Fixed up lots of CSS for flashes and notices.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'recipes#index'
+
   resources :users
   resources :recipes
 
-  root to: 'recipes#index'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
