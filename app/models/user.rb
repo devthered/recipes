c696e7e08c8fa249cd8351f78a75e400a6d8adf9
Devin Reed <reed.devin@gmail.com>
2019-07-23 14:01:00 +0100

    created consisted UI look for user edit, new, and show views

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
 end
