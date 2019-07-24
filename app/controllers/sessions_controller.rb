b79fd3011b2bbe4ab375da42e5ad879c4b8e154f
Devin Reed <reed.devin@gmail.com>
2019-07-24 10:33:59 +0100

    added rest of missing sessions files. tweaked some css around flashes and notices. login now works rudimentarily

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if !!user and !!user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      redirect_to login_path
+    end
+  end
+end
