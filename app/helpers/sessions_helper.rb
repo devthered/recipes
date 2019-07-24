419744ab65295945f64bc0224d61b5bfd15a24a2
Devin Reed <reed.devin@gmail.com>
2019-07-24 14:45:01 +0100

    added remember-me box. styled checkboxes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,23 @@ module SessionsHelper
     session[:user_id] = user.id.to_s
   end
 
+  # Remembers the provided user in a permanent session using cookies.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id.to_s
+    cookies.permanent.signed[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session by deleting cookies.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget current_user
     session[:user_id] = ''
     @current_user = nil
   end
@@ -15,6 +30,12 @@ module SessionsHelper
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      if !!user && !!user.authenticated?(cookies.signed[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
 
+  attr_accessor :remember_token
+
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+  field :remember_digest, type: String
   field :admin, type: Boolean, default: false
 
   has_secure_password
@@ -17,4 +20,33 @@ class User
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  # Generates a new remember token, hashes it, and stores the digest.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Deletes the internal remember digest to forget a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns the hash digest of the provided string
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
