4de95de997a9555cba15961a34162c478fb2e446
Devin Reed <reed.devin@gmail.com>
2019-07-31 12:58:23 +0100

    fixed double dropdown bug. tightened up header and seperated from search bar. search bar follows scroll

--- app/assets/javascripts/recipe-search.js
+++ app/assets/javascripts/recipe-search.js
@@ -9,3 +9,18 @@ $(function() {
 
   update_recipe_index();
 });
+
+var searchBar = document.getElementById("search-bar");
+var topSpacer = document.getElementById("top-spacer");
+var stickyHeight = searchBar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+window.onscroll = function() {
+  if (window.pageYOffset > stickyHeight) {
+    searchBar.classList.add("sticky");
+    topSpacer.classList.add("sticky-spacer");
+  } else {
+    searchBar.classList.remove("sticky");
+    topSpacer.classList.remove("sticky-spacer");
+  }
+};
