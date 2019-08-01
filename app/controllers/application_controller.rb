aae3ed385310a0d3a2dd31f8f4b1220788ab487d
Devin Reed <reed.devin@gmail.com>
2019-08-01 13:24:02 +0100

    root now redirects to signup page. login and signup pages now redirect to each other

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # Confirms a user is logged in. Redirects to log in page otherwise.
+  # Confirms a user is logged in. Redirects to sign up page otherwise.
   def logged_in_user
     unless logged_in?
       store_location
-      redirect_to login_url
+      redirect_to signup_url
     end
   end
 end
