b30a59cabe06b7ba3badf33931695e59f81b2159
Devin Reed <reed.devin@gmail.com>
2019-08-01 13:14:21 +0100

    Login page is now landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # Confirms a user is loggedin.
+  # Confirms a user is logged in. Redirects to log in page otherwise.
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
       redirect_to login_url
     end
   end
