3ad5a990a907a69cabf1b43f7e7d34770b72c26e
Devin Reed <reed.devin@gmail.com>
2019-12-18 16:12:00 +1300

    added default host

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  default_url_options :host => "example.com"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
