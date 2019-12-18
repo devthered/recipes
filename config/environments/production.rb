34d5cee640ac8a5e2052e7f404dd52ee94eb96c3
Devin Reed <reed.devin@gmail.com>
2019-12-18 16:15:27 +1300

    added correct host domain for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "recipes_#{Rails.env}"
 
+  config.action_mailer.default_url_options = { :host => "devin-recipe-cards.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
